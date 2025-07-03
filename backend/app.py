4fa0d09ebf6bc18c30694973e4c4efbc09e31e21
viraj.ap <viraj25092004@gmail.com>
2025-07-03 14:29:38 +0530

    docker

--- backend/app.py
+++ backend/app.py
@@ -37,4 +37,7 @@ def convert():
     return send_file(output_path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
